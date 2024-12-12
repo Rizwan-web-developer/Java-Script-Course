cd65bd2bd82ec21d596249108e3b55c8fa1c6963
Muhammad Rizwan <rk3113798@gmail.com>
2024-12-12 20:55:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+var name = "Ali"
+
+document.write(name)
+
+alert("Saifulah bhag jao")
+confirm("are you human")
+
+varubles = var ,let ,const
+
+let fatherName = "azam"
+let rollNumber = 45
+console.log(rollNumber)
+console.log(fatherName)
+
+let studentName = prompt("Enter your Name")
+console.log(studentName)
+document.write(studentName)
+
+let num1 = 5
+let num2 = 10
+console.log(num1 + num2)
+
+let userNum = prompt("Enter your first number") 
+"55"
+let userNum2 = prompt("Enter your first number") 
+"45" concatinate example :ali azam
+
+let userNum = +prompt("Enter your first number") //"55"
+let userNum2 = +prompt("Enter your secend number") //"45"
+
+
+let minus1 = +prompt("Enter your first number") //"55"
+let minus2 = +prompt("Enter your secend number") //"45"
+console.log(userNum + userNum2 ,minus1 - minus2)
+
+addition example = 100
 // var ,let ,const
 // var old
 // datatype ,string ,number,Boolean (true ,false),object ,array ,symbol 
