0d0b6754d63473883838dc89be30f034822fc920
Muhammad Rizwan <rk3113798@gmail.com>
2024-12-12 20:54:24 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-// var name = "Ali"
+// var ,let ,const
+// var old
+// datatype ,string ,number,Boolean (true ,false),object ,array ,symbol 
 
-// document.write(name)
+let name = "muzammil";
+let fname = "Ishtiyaq";
 
-// alert("Saifulah bhag jao")
+console.log(name + fname)
+// concatinate 
+let rollNum = 12;
+let Classin = true;
+
+let fruit = [12 ,"rizwan","saifullal" ,false]
+
+// console.log(typeof name ,typeof rollNum)
+// console.log(fruit)
+
+// alert("Dangeros website")
 // confirm("are you human")
 
-// varubles = var ,let ,const
+// let fatherName = prompt("Enter your father name")
+// console.log(fatherName);
 
-// let fatherName = "azam"
-// let rollNumber = 45
-// console.log(rollNumber)
-// console.log(fatherName)
-
-// let studentName = prompt("Enter your Name")
-// console.log(studentName)
-// document.write(studentName)
-
-// let num1 = 5
-// let num2 = 10
+// let num1 = 150;
+// let num2 = 50;
 // console.log(num1 + num2)
 
-// let userNum = prompt("Enter your first number") 
-//"55"
-// let userNum2 = prompt("Enter your first number") 
-//"45" concatinate example :ali azam
+let num1 = +prompt("Enter your first Number"); //200 "200"
+let num2 = +prompt("Enter your Secend Number");
+let num3 = prompt("Enter your third Number");
 
-let userNum = +prompt("Enter your first number") //"55"
-let userNum2 = +prompt("Enter your secend number") //"45"
+console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
 
 
-let minus1 = +prompt("Enter your first number") //"55"
-let minus2 = +prompt("Enter your secend number") //"45"
-console.log(userNum + userNum2 ,minus1 - minus2)
 
-// addition example = 100
